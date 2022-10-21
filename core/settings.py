c087c6789796f389b705cc3380408d5d59fd73c0
mariano cardenas <noreply@noreply.test>
2022-10-20 21:09:22 -0500

    jwt

--- core/settings.py
+++ core/settings.py
@@ -110,6 +110,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
--- core/urls.py
+++ core/urls.py
@@ -9,7 +9,15 @@ import road.urls
 import units.urls
 import user.urls
 
+#JWT auth
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
